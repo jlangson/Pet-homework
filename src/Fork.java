d5e1818c75f2ad543e82651a157b92792e0565f0
Jared Langson <langson.jared@gmail.com>
2020-11-16 20:36:27 -0500

    Finished fork

--- src/Fork.java
+++ src/Fork.java
@@ -3,5 +3,50 @@ public class Fork {
     private String material;
     private double mass; //in grams
 
-    
+    public Fork(){
+        location = "no where";
+        material = "none";
+        mass = 0.0;
+    }
+    public Fork(String location, String material){
+        this.location = location;
+        this.material = material;
+    }
+    public Fork(String location, String material, double mass){
+        this.location = location;
+        this.material = material;
+        this.mass = mass;
+    }
+    //getters
+
+
+    public String getLocation() {
+        return location;
+    }
+    public String getMaterial() {
+        return material;
+    }
+    public double getMass() {
+        return mass;
+    }
+    //setters
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    //methods
+    public boolean isHeavy(){
+        return mass > 250;
+    }
+    public String toString(){
+        return this.location + " " + this.material + " " + " " + String.valueOf(this.mass);
+    }
 }
